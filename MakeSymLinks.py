df71a3aa9b2c033d9859164968aeef029945960b
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-01-23 07:08:28 -0800

    Add .tmux.conf file to set of symlinks to be made.
    Refactor variable names.

--- MakeSymLinks.py
+++ MakeSymLinks.py
@@ -7,12 +7,20 @@ import shutil
 homePath = os.path.expanduser("~")
 # Repo File Structure.
 dotVimrcFilePath = os.path.join(os.getcwd(), "vim/vimrc")
-# Target File Structure.
-vimrcTargetPath = os.path.join(homePath, ".vimrc")
+# Target .vimrc path.
+targetVimrcPath = os.path.join(homePath, ".vimrc")
+
+sourceTmuxConfPath = os.path.join(os.getcwd(), "tmux/tmux.conf")
+# Target .tmux.conf path.
+targetTmuxConfPath = os.path.join(homePath, ".tmux.conf")
 
 print(".vimrc source path: ", dotVimrcFilePath)
-print(".vimrc target path: ", vimrcTargetPath)
-if os.path.isfile(vimrcTargetPath) or os.path.islink(vimrcTargetPath):
+print(".vimrc target path: ", targetVimrcPath)
+print(".tmux.conf source path: ", sourceTmuxConfPath)
+print(".tmux.conf target path: ", targetTmuxConfPath)
+if os.path.isfile(targetVimrcPath) or os.path.islink(targetVimrcPath)\
+        or os.path.isfile(targetTmuxConfPath)\
+        or os.path.islink(targetTmuxConfPath):
     # Once a second updates of dotfiles is probably ok.
     # TODO: Append a guid if the backupfolder already exists.
     currentTime = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S')
@@ -20,6 +28,8 @@ if os.path.isfile(vimrcTargetPath) or os.path.islink(vimrcTargetPath):
     backupPath = os.path.join(homePath, backupDir)
     os.mkdir(backupPath)
     print("cleaning up old files into directory: ", backupPath)
-    shutil.move(vimrcTargetPath, backupPath)
+    shutil.move(targetVimrcPath, backupPath)
+    shutil.move(targetTmuxConfPath, backupPath)
     
-os.symlink(dotVimrcFilePath, vimrcTargetPath)
+os.symlink(dotVimrcFilePath, targetVimrcPath)
+os.symlink(sourceTmuxConfPath, targetTmuxConfPath)
