1177ba86435709bf9b134b3674c6664b732d68aa
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2015-02-15 11:45:18 -0800

    Refactor symlink maker into a class.

--- MakeSymLinks.py
+++ MakeSymLinks.py
@@ -4,32 +4,31 @@ import os.path
 import datetime
 import shutil
 
+class dotfile:
+    def __init__(self, fullRepoPath, fullDeploymentPath):
+        self.fullDeploymentPath = fullDeploymentPath
+        self.fullRepoPath = fullRepoPath
+
+    def backup(self, fullPath):
+        baseFolder = os.path.basename(os.path.normpath(fullPath))
+        currentTime = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S')
+        backupFolder = currentTime + baseFolder
+        # Once per second updates of dotfiles is probably ok.
+        # TODO: Append a guid if the backupfolder already exists.
+        backupPath = os.path.join(os.path.expanduser("~"), backupFolder)
+        os.mkdir(backupPath)
+        print("cleaning up old files into directory: ", backupPath)
+        shutil.move(self.fullDeploymentPath, backupPath)
+
+    def makeLink(self):
+        if os.path.isfile(self.fullDeploymentPath):
+            self.backup(self.fullDeploymentPath)
+        os.symlink(self.fullRepoPath, self.fullDeploymentPath)
+
 homePath = os.path.expanduser("~")
-# Repo File Structure.
-dotVimrcFilePath = os.path.join(os.getcwd(), "vim/vimrc")
-# Target .vimrc path.
-targetVimrcPath = os.path.join(homePath, ".vimrc")
-
-sourceTmuxConfPath = os.path.join(os.getcwd(), "tmux/tmux.conf")
-# Target .tmux.conf path.
-targetTmuxConfPath = os.path.join(homePath, ".tmux.conf")
-
-print(".vimrc source path: ", dotVimrcFilePath)
-print(".vimrc target path: ", targetVimrcPath)
-print(".tmux.conf source path: ", sourceTmuxConfPath)
-print(".tmux.conf target path: ", targetTmuxConfPath)
-if os.path.isfile(targetVimrcPath) or os.path.islink(targetVimrcPath)\
-        or os.path.isfile(targetTmuxConfPath)\
-        or os.path.islink(targetTmuxConfPath):
-    # Once a second updates of dotfiles is probably ok.
-    # TODO: Append a guid if the backupfolder already exists.
-    currentTime = datetime.datetime.now().strftime('%Y-%m-%d_%H%M%S')
-    backupDir = currentTime + "_dotfilesBackup"
-    backupPath = os.path.join(homePath, backupDir)
-    os.mkdir(backupPath)
-    print("cleaning up old files into directory: ", backupPath)
-    shutil.move(targetVimrcPath, backupPath)
-    shutil.move(targetTmuxConfPath, backupPath)
-    
-os.symlink(dotVimrcFilePath, targetVimrcPath)
-os.symlink(sourceTmuxConfPath, targetTmuxConfPath)
+vimrc = dotfile(os.path.join(os.getcwd(), "vim/vimrc"),
+        os.path.join(homePath, ".vimrc"))
+vimrc.makeLink();
+tmuxConf = dotfile(os.path.join(os.getcwd(), "tmux/tmux.conf"),
+        os.path.join(homePath, ".tmux.conf"))
+tmuxConf.makeLink();
