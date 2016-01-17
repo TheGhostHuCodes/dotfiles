fd4bd69ce139fc908350dfc9f00a1a3f48b72803
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-01-17 07:58:07 -0800

    Add shell script for setting up Ubuntu machine

---
+++ setup/setup_ubuntu.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# run apt-get update if you haven't in the last 2 hours.
+if test `find "/var/lib/apt/periodic/update-success-stamp" -mmin +120`
+then
+  apt-get update
+fi
+
+# Terminal tools.
+apt-get install zsh
+apt-get install vim
+apt-get install tmux
+apt-get install tree
+
+# Desktop usability.
+apt-get install chromium-browser
+apt-get install gnome-tweak-tool
+
+# Version control.
+apt-get install git
+apt-get install openssh-server
+
+# For building unison.
+apt-get install ocaml liblablgtk2-ocaml-dev
