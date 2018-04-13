b9460ac671ed48390bfc2a180abb964709511777
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-04-13 15:20:28 -0700

    Install latest version of ansible on ubuntu

--- setup/ubuntu/bootstrap_ansible.sh
+++ setup/ubuntu/bootstrap_ansible.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+apt-add-repository --yes ppa:ansible/ansible
 apt-get update
 apt-get --yes install ansible
+
+if ! grep "localhost ansible_connection=local" /etc/ansible/hosts
+then
+    echo "localhost ansible_connection=local" >> /etc/ansible/hosts
+fi
