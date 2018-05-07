e936a850affe576498905847e06d852cf1e67287
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-05-07 12:41:50 -0700

    Update vscode golang extension

--- setup/scripts/vscode/extensions.sh
+++ setup/scripts/vscode/extensions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension lukehoban.Go
+code --install-extension ms-vscode.Go
 code --install-extension mike-lischke.vscode-antlr4
 code --install-extension stkb.rewrap
 code --install-extension vscodevim.vim
