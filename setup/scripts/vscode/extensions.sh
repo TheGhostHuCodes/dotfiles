b0be0c3c4339a53a5fbeb84890324378fc506ad5
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-05-21 10:19:34 -0700

    Refactor extensions.sh script

--- setup/scripts/vscode/extensions.sh
+++ setup/scripts/vscode/extensions.sh
@@ -3,7 +3,7 @@
 code --install-extension Arjun.swagger-viewer
 code --install-extension avli.clojure
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension ms-vscode.Go
 code --install-extension mike-lischke.vscode-antlr4
+code --install-extension ms-vscode.Go
 code --install-extension stkb.rewrap
 code --install-extension vscodevim.vim
