576c22ef13e9c9553b3128b545615eedb2300296
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-05-22 10:53:05 -0700

    Add code-runner to vscode extensions

--- setup/scripts/vscode/extensions.sh
+++ setup/scripts/vscode/extensions.sh
@@ -3,6 +3,7 @@
 code --install-extension Arjun.swagger-viewer
 code --install-extension avli.clojure
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension formulahendry.code-runner
 code --install-extension mike-lischke.vscode-antlr4
 code --install-extension ms-vscode.Go
 code --install-extension stkb.rewrap
