3c36c33458b4f0eaa7896199d83e17bbaee8a20e
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-05-29 16:37:39 -0700

    Add python extension to vscode extensions

--- setup/scripts/vscode/extensions.sh
+++ setup/scripts/vscode/extensions.sh
@@ -5,6 +5,7 @@ code --install-extension avli.clojure
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension formulahendry.code-runner
 code --install-extension mike-lischke.vscode-antlr4
+code --install-extension ms-python.python
 code --install-extension ms-vscode.Go
 code --install-extension stkb.rewrap
 code --install-extension vscodevim.vim
