e6e00f67a926c5c1f53665b321e5aabfc32497f3
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2018-08-22 20:41:59 -0700

    Update vscode scripts to choose yes when installing extensions

--- setup/scripts/vscode/extensions.sh
+++ setup/scripts/vscode/extensions.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-code --install-extension Arjun.swagger-viewer
-code --install-extension avli.clojure
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension formulahendry.code-runner
-code --install-extension justusadam.language-haskell
-code --install-extension mauve.terraform
-code --install-extension mike-lischke.vscode-antlr4
-code --install-extension ms-python.python
-code --install-extension ms-vscode.Go
-code --install-extension stkb.rewrap
-code --install-extension vscodevim.vim
+echo "yes" | code --install-extension Arjun.swagger-viewer
+echo "yes" | code --install-extension avli.clojure
+echo "yes" | code --install-extension dbaeumer.vscode-eslint
+echo "yes" | code --install-extension formulahendry.code-runner
+echo "yes" | code --install-extension justusadam.language-haskell
+echo "yes" | code --install-extension mauve.terraform
+echo "yes" | code --install-extension mike-lischke.vscode-antlr4
+echo "yes" | code --install-extension ms-python.python
+echo "yes" | code --install-extension ms-vscode.Go
+echo "yes" | code --install-extension stkb.rewrap
+echo "yes" | code --install-extension vscodevim.vim
