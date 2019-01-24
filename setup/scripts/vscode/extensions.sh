527c6692d7c7a086b65ecc08642755e21fbaa0b6
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-01-24 09:57:48 -0800

    Update VSCode extensions
    - kalitaalexey.vscode-rust and rust-lang.rust for Rust programming.
    - njpwerner.autodocstring for generating docstring templates for Python.
    - sbrink.elm for Elm programming.
    - zxh404.vscode-proto3 for protobuf schema editing.

--- setup/scripts/vscode/extensions.sh
+++ setup/scripts/vscode/extensions.sh
@@ -5,11 +5,16 @@ echo "yes" | code --install-extension avli.clojure
 echo "yes" | code --install-extension dbaeumer.vscode-eslint
 echo "yes" | code --install-extension formulahendry.code-runner
 echo "yes" | code --install-extension justusadam.language-haskell
+echo "yes" | code --install-extension kalitaalexey.vscode-rust
 echo "yes" | code --install-extension mauve.terraform
 echo "yes" | code --install-extension mike-lischke.vscode-antlr4
 echo "yes" | code --install-extension ms-python.python
 echo "yes" | code --install-extension ms-vscode.Go
+echo "yes" | code --install-extension njpwerner.autodocstring
 echo "yes" | code --install-extension PeterJausovec.vscode-docker
+echo "yes" | code --install-extension rust-lang.rust
+echo "yes" | code --install-extension sbrink.elm
 echo "yes" | code --install-extension stkb.rewrap
 echo "yes" | code --install-extension streetsidesoftware.code-spell-checker
 echo "yes" | code --install-extension vscodevim.vim
+echo "yes" | code --install-extension zxh404.vscode-proto3
