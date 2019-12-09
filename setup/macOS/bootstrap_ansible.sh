cd269fd48c0bd143637674fe08255d663f184d43
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-12-09 14:39:53 -0800

    Fix formatting for macOS bootstrap_ansible shell script

--- setup/macOS/bootstrap_ansible.sh
+++ setup/macOS/bootstrap_ansible.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-if command -v xcode-select > /dev/null 2>&1
-then
+if command -v xcode-select >/dev/null 2>&1; then
 	echo "xcode-select is already installed!"
 else
-  xcode-select --install
+	xcode-select --install
 fi
 
-if command -v brew > /dev/null 2>&1
-then
+if command -v brew >/dev/null 2>&1; then
 	echo "Brew is already installed!"
 else
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
