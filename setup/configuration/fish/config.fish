b426137c5aa5f01467cf78de337bb4b8717ac8a0
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2019-12-11 20:58:11 -0800

    Update macOS system installation
    - Add fish configuration to support local exports, aliases, and pyenv.
    - Remove unused or duplicated commandline tools.
    - Add homebrew font tap.
    - Remove Java8 and packages depending on it.
    - Remove Golang dep in favor of using built-in modules.
    - Remove Haskell tools except for Stack.
    - Remove Node.
    - Remove exercism.
    - Remove local postgresql installation in favor of using containers.
    - Remove Vundle and vim configuration file deployment.

---
+++ setup/configuration/fish/config.fish
@@ -0,0 +1,8 @@
+if test -e ~/.local_exports
+    source ~/.local_exports
+end
+
+alias vi="nvim"
+
+# https://angristan.xyz/how-to-use-nvm-rbenv-pyenv-goenv-with-fish-shell/
+source (pyenv init - | psub)
