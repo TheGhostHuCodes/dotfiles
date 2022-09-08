bfb0e619078c9ae7b81d3cde051348beee2490d3
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2022-09-07 20:54:49 -0700

    Add neovim configuration to dotter

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+-- LEADER
+vim.g.mapleader = ","
+vim.g.localleader = "\\"
+
+-- IMPORTS
+require("vars")
+require("opts")
+-- require('keys')
+-- require('plug')
---
+++ nvim/lua/opts.lua
@@ -0,0 +1,33 @@
+local opt = vim.opt
+
+-- [[ Context ]]
+opt.colorcolumn = "80"
+opt.number = true
+opt.relativenumber = true
+-- Minimal number of screen lines to keep above and below the cursor.
+opt.scrolloff = 4
+opt.signcolumn = "yes"
+
+-- [[ Filetypes ]]
+opt.encoding = "utf8"
+opt.fileencoding = "utf8"
+
+-- [[ Theme ]]
+opt.syntax = "ON"
+opt.termguicolors = true
+
+-- [[ Search ]]
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+opt.hlsearch = true
+
+-- [[ Whitespace ]]
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.tabstop = 4
+
+-- [[ Splits ]]
+opt.splitright = true
+opt.splitbelow = true
