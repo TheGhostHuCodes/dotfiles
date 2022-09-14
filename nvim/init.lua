61442c7c4b35d5d4b996a7d48669fa1912ed0c57
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2022-09-13 22:08:51 -0700

    Add nvim-orgmode plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,9 +9,33 @@ require("keymappings")
 require("plugins")
 
 -- PLUGINS
+-- nvim-tree setup.
 require("nvim-tree").setup()
+
+-- lualine setup.
 require("lualine").setup({
 	options = {
 		theme = "solarized_dark",
 	},
 })
+
+-- nvim-orgmode setup.
+-- Load custom tree-sitter grammar for org filetype
+require("orgmode").setup_ts_grammar()
+-- Tree-sitter configuration
+require("nvim-treesitter.configs").setup({
+	-- If TS highlights are not enabled at all, or disabled via `disable` prop,
+	-- highlighting will fallback to default Vim syntax highlighting.
+	highlight = {
+		enable = true,
+		-- Required for spellcheck, some LaTeX highlights and code block
+		-- highlights that do not have ts grammar.
+		additional_vim_regex_highlighting = { "org" },
+	},
+	ensure_installed = { "org", "python", "rust" }, -- Or run :TSUpdate org
+})
+require("orgmode").setup({
+	org_agenda_files = "~/org/*",
+	org_default_notes_file = "~/org/refile.org",
+})
+require("orgmode").setup()
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,13 +1,25 @@
 require("packer").startup({
 	function(use)
-		-- [[ Put Plugins Here ]]
+		-- neovim package manager written in lua.
 		use("wbthomason/packer.nvim")
+		-- neovim filetree UI.
 		use({
 			"kyazdani42/nvim-tree.lua",
 			requires = "kyazdani42/nvim-web-devicons",
 		})
+		-- neovim status line.
 		use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons", opt = true } })
+		-- Solarized color scheme.
 		use("overcache/NeoSolarized")
+		-- Treesitter for better parsing.
+		use({ "nvim-treesitter/nvim-treesitter" })
+		-- neovim port of orgmode.
+		use({
+			"nvim-orgmode/orgmode",
+			config = function()
+				require("orgmode").setup({})
+			end,
+		})
 	end,
 	config = {
 		package_root = vim.fn.stdpath("config") .. "/site/pack",
